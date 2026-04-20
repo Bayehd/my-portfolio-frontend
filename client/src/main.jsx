615592da291789dbabff0c557a591be18cbc3ade
“Bayehd” <91498301+Bayehd@users.noreply.github.com>
2026-04-20 13:15:31 -0400

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'  // ← Changed from BrowserRouter
 import App from './App'
 import './styles/index.css'
 
 // Get the root element from the DOM where React will mount
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* BrowserRouter enables client-side routing throughout the app */}
-    <BrowserRouter>
+    {/* HashRouter enables client-side routing with hash-based URLs */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
